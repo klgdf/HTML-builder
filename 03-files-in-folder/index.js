6496c5799df49fa6bd72ffd7fcd949482211d0e1
klgdf <Azzaliashc@gmail.com>
2022-05-25 10:18:41 +0400

    feat: add the solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+  if (error) throw Error;
+  files.forEach(file => {
+    if (file.isFile()) {
+      const fName = file.name.split('.')[0];
+      const fExt = file.name.split('.')[1];
+
+      fs.stat(path.join(folderPath, file.name), (err, stats) => {
+        if (error) throw Error;
+        console.log(fName + ' - ' + fExt + ' - ' + stats.size + 'b');
+      });
+    }
+  });
+});
